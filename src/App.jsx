ca5a46b8e7cb3eb9baa7cac5148c38c0e565ec82
Katerina <Kateryna.e.nikitina@gmail.com>
2023-11-20 13:03:33 -0700

    running added

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,37 @@ import "./App.css";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [running, setRunning] = useState(false);
 
   const handleReset = () => {
     setCounter(0);
   };
 
+  const toggleRunning = () => {
+    setRunning(prevRunning => !prevRunning);
+  };
+
   useEffect(() => {
     console.log("useEffect starting");
-    let timerId = setInterval(() => {
+    let timerId;
+
+   if (running) {
+    timerId = setInterval(() => {
       setCounter(prevCount => prevCount + 1);
     }, 3000);
-    
+   }
     return () => {
       clearInterval(timerId);
     };
-  }, []);
+  }, [running]);
   return (
     <>
       <h1>Monday Assignment</h1>
       <div className="counter">Counter: {counter}</div>
       <div className="reset-button-div">
-        <button onClick={handleReset}>RESET</button>
+      <button onClick={toggleRunning}>START</button>
+      <button onClick={toggleRunning}>STOP</button>
+      <button onClick={handleReset}>RESET</button>
       </div>
     </>
   );
